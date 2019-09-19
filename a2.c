91b4f76f0c83bc2c0247393e90a869bdef86c8c2
Dylan Richards <dylan.richards81@gmail.com>
2019-09-18 22:29:09 -0400

    Finished implimentation

--- a2.c
+++ a2.c
@@ -4,17 +4,19 @@
 	Due: September 23, 2019		Started: September 12, 2019
 	Credit: 10 points
 
-	Problem:
+	Problem: Create two linked lists of random numbers in a specified range. One list for even numbers, the other for odd numbers.
+	If the random number generated is already in the linked list, delete it.
 
-	Solution:
+	Solution: Created two linked lists with the lnode struct. Determined if the random number was odd or even.
+	Then searched corresponding list. If found, delete the node, otherwise add random number to list.
 
-	Data-structure used:
+	Data-structure used: struct lnode with two members - a value and a pointer to the next lnode in the list.
 
-	Accessing functions for the data structure:
+	Accessing functions for the data structure: Standard C functions for accessing structs and modules.
 
 	Errors handled: Command line arguments handled. If no arguments are specified, the program closes.
 
-	Limitations:
+	Limitations: Normal system limitations on memory.
 
 	Acknowledgment: I wrote the program for Rattan's CPS360 class.
 */
@@ -29,7 +31,12 @@ struct lnode {int value; struct lnode *next;};
 
 typedef struct lnode lnode;
 
-/*
+/*	Check if proper arguments were supplied. If not, print proper usage.
+	Get repeated count from command line argument.
+	Seed the random number generator with the system clock.
+	Generate a random number and determine if it is even or odd. Insert number in list.
+	Continue that process as specified in the command line argument.
+	Print both lists and return the memory from the lists back to they system.
 */
 int main(int argc, char *argv[]){
 	if(argc < 2){
@@ -55,10 +62,8 @@ int main(int argc, char *argv[]){
 		int num = nextnum();
 
 		if(num & 1){
-			printf("Odd: %d\n", num);
 			if(!insertinlist(lodds, num)) printf("ERROR Inserting");
 		} else {
-			printf("Even: %d\n", num);	
 			if(!insertinlist(levens, num)) printf("ERROR Inserting");
 		}
 	}
@@ -110,7 +115,7 @@ void search(lnode *list, lnode **crnt, lnode **pred, int x){
 	*crnt = list;
 	*pred = NULL;
 	while(*crnt){
-		if((*crnt) -> value == x) return;
+		if((*crnt) -> value == x)return;
 		*pred = *crnt;
 		*crnt = (*crnt) -> next;
 	}
@@ -121,16 +126,28 @@ void search(lnode *list, lnode **crnt, lnode **pred, int x){
 */
 int insertinlist(lnode **list, int x){
 	void search(lnode *list, lnode **crnt, lnode **pred, int x);
-	
-	lnode *prev = *list;
-	
-	void getnode(lnode **ptr);
-	getnode(list);
-	
-	if(*list == NULL) return 0;
+	lnode *crntptr, **crnt;
+	lnode *predptr, **pred;
 
-	(*list) -> value = x;
-	(*list) -> next = prev;
+	crnt = &crntptr;
+	pred = &predptr;
+
+	lnode *prev = *list;
+
+	search(*list, crnt, pred, x);
+
+	if(*crnt){
+		int delfromlist(lnode **list, lnode *after);
+		return delfromlist(list, predptr);
+	} else {
+		void getnode(lnode **ptr);
+		getnode(list);
+	
+		if(!(*list)) return 0;
+
+		(*list) -> value = x;
+		(*list) -> next = prev;
+	}
 	
 	return 1;
 }
@@ -138,21 +155,33 @@ int insertinlist(lnode **list, int x){
 /*	Deletes the node from list that is successor of the node pointed by after.
 	Returns 1 on success, 0 otherwise.
 */
-int delfromlist(lnode **list, lnode *after);
+int delfromlist(lnode **list, lnode *after){
+	lnode *deletenode;	
+
+	if(!after){
+		deletenode = *list;
+		*list = deletenode -> next;
+	} else {
+		deletenode = after -> next;
+		after -> next = deletenode -> next;
+	}
+
+	void freenode(lnode **ptr);
+	freenode(&deletenode);
+	return 1;
+}
 
 /*	Prints the contents of given list (on one line on stdout)
 */
 void printlist(lnode *list){
-	if(list == NULL){
+	if(!list){
 		printf("empty\n");
 		return;
 	}
 
-	printf("%d", list -> value);
-	
-	while(list -> next != NULL){
+	while(list){
+		printf(" %d ", list -> value);
 		list = list -> next;
-		printf(", %d", list -> value);
 	}
 
 	printf("\n");
